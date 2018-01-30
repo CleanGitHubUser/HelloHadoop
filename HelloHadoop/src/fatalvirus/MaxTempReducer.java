b511adf526f292523fd85d9ec0810cd0c2cf46ad
fatalwhite <jeewoong92@hanmail.net>
2018-01-30 16:59:09 +0900

    2018-01-30

--- HelloHadoop/src/fatalvirus/MaxTempReducer.java
+++ HelloHadoop/src/fatalvirus/MaxTempReducer.java
@@ -1,7 +1,35 @@
 package fatalvirus;
 
-public class MaxTempReducer {
+import java.io.IOException;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MaxTempReducer 
+	extends Reducer<Text, IntWritable, Text, IntWritable> {
+	
+	@Override
+	protected void reduce(Text key, 
+			Iterable<IntWritable> values,
+			Context context) 
+					throws IOException, InterruptedException {
+		
+		// 최고온도를 0이로 설정
+		int maxValue = Integer.MIN_VALUE;
+		
+		// 맵의 결과로 추출된 년도와 온도가 들어 있는
+		// values 를 for문으로 순환하며
+		// 최고온도를 조사
+		for (IntWritable value : values) {
+			maxValue = Math.max(maxValue, value.get());
+		}
+		
+		// 최고 온도를 결과로 출력
+		context.write(key,
+				new IntWritable(maxValue));
+		
+	}
 
 
 }
